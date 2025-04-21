415e0e497a15dd53098200a7274eccc1b364e9ee
Durga-velu <durgadevi134babu@gmail.com>
2025-04-21 23:35:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+
+
+
+let promise1=new Promise((resolve,reject){
+
+	setTimeOut(()=>{
+		
+		resolve(Promise 1, 2)
+		
+	},2000)
+})
+
+
+let promise2=new Promise((resolve,reject){
+
+	setTimeOut(()=>{
+		
+		resolve(Promise 2, 1)
+		
+	},2000)
+})
+
+
+
+let promise3=new Promise((resolve,reject){
+
+	setTimeOut(()=>{
+		
+		resolve(Promise 3, 3)
+		
+	},2000)
+})
+
+let output= document.getElementById("output")
+Promise.all([promise1,promise1,promise3]).then((text, seconds) =>{
+	let row=document.createElement("tr");
+	output.appendChild(row);
+	let col1=document.createElement("td");
+	row.appendChild(col1)
+	col1.textContent=text;
+	let col2=document.createElement("td");
+	col2.textContent=seconds
+	row.appendChild(col2);
+})
